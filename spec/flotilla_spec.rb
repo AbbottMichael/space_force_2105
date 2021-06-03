6dd375af06d1e686f55b1b717058327ad3db50a0
Michael Abbott <michael@mjabbottdesign.com>
2021-06-03 09:32:15 -0700

    Add Flotilla #recommend_personnel test

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -23,23 +23,44 @@ RSpec.describe Flotilla do
 
   describe 'method' do
 
-    it 'adds ship' do
-      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+    before :each do
+      @daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      @daedalus.add_requirement({astrophysics: 6})
+      @daedalus.add_requirement({quantum_mechanics: 3})
+      @kathy = Person.new('Kathy Chan', 10)
+      @kathy.add_specialty(:astrophysics)
+      @kathy.add_specialty(:quantum_mechanics)
+      @polly = Person.new('Polly Parker', 4)
+      @polly.add_specialty(:operations)
+      @polly.add_specialty(:maintenance)
+      @rover = Person.new('Rover Henriette', 1)
+      @polly.add_specialty(:operations)
+      @polly.add_specialty(:maintenance)
+      @sampson = Person.new('Sampson Edwards', 7)
+      @polly.add_specialty(:astrophysics)
+      @polly.add_specialty(:quantum_mechanics)
+    end
 
-      expect(@seventh_flotilla.add_ship(daedalus)).to eq([daedalus])
+    it 'adds ship' do
+      expect(@seventh_flotilla.add_ship(@daedalus)).to eq([@daedalus])
     end
 
     it 'adds personnel' do
-      kathy = Person.new('Kathy Chan', 10)
-      polly = Person.new('Polly Parker', 4)
-      rover = Person.new('Rover Henriette', 1)
-      sampson = Person.new('Sampson Edwards', 7)
-      @seventh_flotilla.add_personnel(kathy)
-      @seventh_flotilla.add_personnel(polly)
-      @seventh_flotilla.add_personnel(rover)
-      @seventh_flotilla.add_personnel(sampson)
+      @seventh_flotilla.add_personnel(@kathy)
+      @seventh_flotilla.add_personnel(@polly)
+      @seventh_flotilla.add_personnel(@rover)
+      @seventh_flotilla.add_personnel(@sampson)
 
-      expect(@seventh_flotilla.personnel).to eq([kathy, polly, rover, sampson])
+      expect(@seventh_flotilla.personnel).to eq([@kathy, @polly, @rover, @sampson])
+    end
+
+    it 'recommends personnel' do
+      @seventh_flotilla.add_personnel(@kathy)
+      @seventh_flotilla.add_personnel(@polly)
+      @seventh_flotilla.add_personnel(@rover)
+      @seventh_flotilla.add_personnel(@sampson)
+
+      expect(@seventh_flotilla.recommend_personnel(@daedalus)).to eq([@kathy, @sampson])
     end
   end
 end
