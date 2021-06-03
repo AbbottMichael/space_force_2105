f76b7ce89020d431430fb385ccbe05323c9bc799
Michael Abbott <michael@mjabbottdesign.com>
2021-06-03 09:13:56 -0700

    Add Flotilla #add_personnel test

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -24,10 +24,22 @@ RSpec.describe Flotilla do
   describe 'method' do
 
     it 'adds ship' do
-
       daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
 
       expect(@seventh_flotilla.add_ship(daedalus)).to eq([daedalus])
+    end
+
+    it 'adds personnel' do
+      kathy = Person.new('Kathy Chan', 10)
+      polly = Person.new('Polly Parker', 4)
+      rover = Person.new('Rover Henriette', 1)
+      sampson = Person.new('Sampson Edwards', 7)
+      @seventh_flotilla.add_personnel(kathy)
+      @seventh_flotilla.add_personnel(polly)
+      @seventh_flotilla.add_personnel(rover)
+      @seventh_flotilla.add_personnel(sampson)
+
+      expect(@seventh_flotilla.personnel).to eq([kathy, polly, rover, sampson])
     end
   end
 end
