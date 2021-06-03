a3324a3cdce82d890a14b243bf6321d93d6b89e6
Michael Abbott <michael@mjabbottdesign.com>
2021-06-03 08:46:02 -0700

    Add Spacecraft #add_requirement code

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,8 +1,15 @@
 class Spacecraft
-  attr_reader :name, :fuel
-  
+  attr_reader :name,
+              :fuel,
+              :requirements
+
   def initialize(data_hash)
     @name = data_hash[:name]
     @fuel = data_hash[:fuel]
+    @requirements = []
+  end
+
+  def add_requirement(data_hash)
+    @requirements << data_hash
   end
 end
