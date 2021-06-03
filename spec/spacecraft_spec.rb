b5807fd45be2959d1fb413489d83fac96f46965d
Michael Abbott <michael@mjabbottdesign.com>
2021-06-03 08:43:26 -0700

    Add Spacecraft #add_requirement test

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -1,18 +1,31 @@
 require './lib/spacecraft'
 
 RSpec.describe Spacecraft do
-  describe 'instantiation' do
-    it 'exists' do
-      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
 
-      expect(daedalus).to be_a(Spacecraft)
+  before :each do
+    @daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+  end
+
+  describe 'instantiation' do
+
+    it 'exists' do
+      expect(@daedalus).to be_a(Spacecraft)
     end
 
     it 'has readable attributes' do
-      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      expect(@daedalus.name).to eq('Daedalus')
+      expect(@daedalus.fuel).to eq(400)
+      expect(@daedalus.requirements).to eq([])
+    end
+  end
 
-      expect(daedalus.name).to eq('Daedalus')
-      expect(daedalus.fuel).to eq(400)
+  describe 'methods' do
+
+    it 'add requirements' do
+      @daedalus.add_requirement({astrophysics: 6})
+      @daedalus.add_requirement({quantum_mechanics: 3})
+
+      expect(@daedalus.requirements).to eq({astrophysics: 6}, {quantum_mechanics: 3})
     end
   end
 end
